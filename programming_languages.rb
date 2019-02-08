b1f272372785d3306ab0f83e1ff87d94e89e2ea0
Adam Gordon <adamjgordon24@gmail.com>
2019-02-08 06:47:36 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  new_languages = {}
+  languages.each do |style, code|
+    code.each do |name, attribute|
+      if new_languages[name] == nil  
+         new_languages[name] = attribute
+      end
+      new_languages[name][:style] = []
+      new_languages[name][:style] << style
+   end
+  end
+  new_languages[:javascript][:style] << :oo
+  new_languages
 end
