16fde003c4ee3fb5e2aecb43353daa7b0d4d8c32
dlee16 <diana.lee116@gmail.com>
2019-02-11 13:09:06 -0500

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,15 @@
+require "pry"
+
 def reformat_languages(languages)
-  # your code here
+  hash={}
+  languages.each do |key, value|
+    value.each do |name, data|
+      if hash[name]
+        hash[name][:style]<< key
+      else
+        hash[name] = {type: data[:type], style: [key]}
+      end
+    end
+  end
+  hash
 end
